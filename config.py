1215c3c7b7f6ae6886ad6370610df11eadd98a7b
Brazzard <verba@steelkiwi.com>
2017-11-06 11:51:43 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,22 @@
+# search keys
+search_word = "Automation"
+search_domain = "testautomationday.com"
+# valid domain "siemens.com"
+
+# selectors
+first_link = "#rso > div:nth-child(1) > div > div:nth-child(1) > div > div > h3 > a"
+search_result_selectors = "#rso h3 a"
+
+# number of pages const
+num_of_pages_to_check = 5
+
+# chrome_31_config = {
+#     'platform': "Mac OS X 10.9",
+#     'browserName': "chrome",
+#     'version': "31",
+# }
+# ff_56_config = {
+#     'platform': "Mac OS X 10.9",
+#     'browserName': "firefox",
+#     'version': "56",
+# }

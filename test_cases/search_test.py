d48c1af9bb7a5aab8000a053edf016a41441b00d
Brazzard <verba@steelkiwi.com>
2017-11-14 23:41:09 +0200

    HtmlTestRunner have been added

--- test_cases/search_test.py
+++ test_cases/search_test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import HtmlTestRunner
 import unittest
 from base_test import BaseTest
 
@@ -13,6 +14,7 @@ class SearchTest(BaseTest):
         self.num_of_pages_to_check = 5
 
     def test_check_first_link_title(self):
+        """ Check the first link on search result """
         self.main_page.perform_search(self.search_word)
         self.search_result_page.first_link().click()
         test_check_first_link_title_result = self.search_word in self.driver.title
@@ -20,6 +22,7 @@ class SearchTest(BaseTest):
                          '"{}" is not found in page title "{}"'.format(self.search_word, self.driver.title))
 
     def test_search_for_expected_domain(self):
+        """ The search of expected domain on the first 5 pages of the search result """
         self.main_page.perform_search(self.search_word)
         # get list of links for 1st page
         result_list = self.search_result_page.search_result_links_on_given_pages(self.num_of_pages_to_check)
@@ -31,4 +34,4 @@ class SearchTest(BaseTest):
                          'There is no expected domain "{}"'.format(self.search_domain))
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='/Users/Brazzard/Desktop/'))
